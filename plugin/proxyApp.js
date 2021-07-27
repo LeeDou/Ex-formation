9396ccc2b58e24618ae40b9bc33dbeba2e2815bb
kantle <oncwnuOTEQhjbT1qXiaGrIMxZ4vg@git.weixin.qq.com>
2021-07-27 15:55:28 +0800

    清空

--- plugin/proxyApp.js
+++
@@ -1,7 +0,0 @@
-var oldApp = App;
-App = function (option) {
-  proxyClick(option, 'onLaunch', 'appLaunch');
-  mp_proxy(option, 'onShow', 'appShow');
-  mp_proxy(option, 'onHide', 'appHide');
-  oldApp.apply(this, arguments);
-};
--- utils/func.js
+++
@@ -1,7 +0,0 @@
-/**
- * author likang@sensorsdata.cn
- * 此模块包含一般的处理函数
- */
-export function trim(str) {
-  return str.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
-}
